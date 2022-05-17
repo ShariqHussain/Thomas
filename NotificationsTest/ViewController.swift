9a1d61ff934a1b239f90d225a4bc2d4525341b1b
Shariq Hussain <shariq04_amu@yahoo.com>
2022-05-17 21:25:39 +0530

    First Commit

--- NotificationsTest/ViewController.swift
+++ NotificationsTest/ViewController.swift
@@ -12,8 +12,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+              if settings.authorizationStatus == .authorized {
+                  // Notifications are allowed
+              }
+              else {
+                  // Either denied or notDetermined
+                  DispatchQueue.main.async {
+                      let alertController = UIAlertController(title: nil, message: "Do you want to change notifications settings?", preferredStyle: .alert)
 
+                      let action1 = UIAlertAction(title: "Settings", style: .default) { (action:UIAlertAction) in
+                          if let appSettings = NSURL(string: UIApplication.openSettingsURLString) {
+                              UIApplication.shared.open(appSettings as URL, options: [:], completionHandler: nil)
+                          }
+                      }
 
+                      let action2 = UIAlertAction(title: "Cancel", style: .cancel) { (action:UIAlertAction) in
+                      }
+
+                      alertController.addAction(action1)
+                      alertController.addAction(action2)
+                      self.present(alertController, animated: true, completion: nil)
+                  }
+              }
+          }
+    }
 }
 
